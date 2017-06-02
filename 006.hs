30d9769cfe567eec6dfacf2b845301bd0e29226a
Will Song <incertia@incertia.net>
2017-06-01 21:05:41 -0500

    ^ is not xor

--- 006.hs
+++ 006.hs
@@ -1,6 +1,6 @@
 main :: IO ()
 main = putStrLn $ show ans
   where ans = abs $ ss - sq
-        ss = sum $ zipWith (*) nums nums
-        sq = let s = sum nums in s * s
+        ss = sum $ (^2) <$> nums
+        sq = (^2) $ sum nums
         nums = [1..100]
