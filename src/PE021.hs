353ba5d770d70f384ee02f97d529a047027fb2f7
Will Song <incertia@incertia.net>
2017-06-28 12:14:41 -0600

    fix amicable

--- src/PE021.hs
+++ src/PE021.hs
@@ -1,6 +1,5 @@
 module PE021 (solve021) where
 
--- TODO: this doesn't work yet
 divisors :: Integer -> [Integer]
 divisors n = [x | x <- [1..n - 1], n `mod` x == 0]
 
@@ -8,7 +7,7 @@ d :: Integer -> Integer
 d = sum . divisors
 
 amicable :: Integer -> Bool
-amicable n = d n' == n
+amicable n = n /= n' && d n' == n
   where n' = d n
 
 solve021 :: IO ()
