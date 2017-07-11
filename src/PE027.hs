33b5610a461671d25931b0a947a0988c10bb8153
Will Song <songw@ainfosec.com>
2017-07-11 14:17:39 -0600

    problem 27

---
+++ src/PE027.hs
@@ -0,0 +1,16 @@
+module PE027 (solve027) where
+
+import Control.Arrow ((&&&))
+import Data.List (maximumBy)
+import Data.Numbers.Primes (primes, isPrime)
+import Data.Ord (comparing)
+
+countPrimes :: Integer -> Integer -> Integer
+countPrimes a b = toInteger . length $ takeWhile isPrime (f <$> [0..])
+  where f x = x^2 + a * x + b
+
+solve027 :: String -> Integer
+solve027 _ = a * b
+  where vals = (,) <$> [-999..999] <*> takeWhile (<=1000) primes
+        ((a, b), _) = maximumBy (comparing snd) stuff
+        stuff = (id &&& uncurry countPrimes) <$> vals
