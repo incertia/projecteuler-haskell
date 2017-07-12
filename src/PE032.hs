a9705e7d16f2d7c18b74fd604f7c3520b5d45880
Will Song <songw@ainfosec.com>
2017-07-12 12:25:13 -0600

    PE031 -> PE032

--- src/PE032.hs
+++ src/PE032.hs
@@ -1,4 +1,4 @@
-module PE031 (solve031) where
+module PE032 (solve032) where
 
 import Control.Arrow ((&&&), (>>>))
 import Data.List (group, sort)
@@ -7,8 +7,7 @@ pandigital :: Integer -> Bool
 pandigital = or . fmap cond . prods
   where prods n = [[a, n `div` a, n] | a <- [1..n], n `mod` a == 0]
         cond :: [Integer] -> Bool
-        cond = concatMap show >>> ((sort >>> group >>> length >>> (==9)) &&&
-                                   notElem '0') >>> uncurry (&&)
+        cond = (=="123456789") . sort . concatMap show
 
 solve032 :: String -> Integer
 solve032 _ = sum . filter pandigital $ [1234..9876]
