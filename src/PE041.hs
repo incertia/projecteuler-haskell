52071c067dddfae175c767e72f398f6fe73f5a19
Will Song <songw@ainfosec.com>
2017-07-12 14:09:31 -0600

    problem 41

---
+++ src/PE041.hs
@@ -0,0 +1,13 @@
+module PE041 (solve041) where
+
+import Data.List (sort)
+import Data.Numbers.Primes (primes)
+
+-- abuse the fact that String = [Char]
+pandigital :: Integer -> Bool
+pandigital n = (== take (length (show n)) (iterate succ '1')) . sort . show $ n
+
+-- notice that sum [1..8] = 36 and sum [1..9] = 45 so 8,9-pandigital numbers are
+-- divisible by 9
+solve041 :: String -> Integer
+solve041 _ = maximum . filter pandigital . takeWhile (< 10^7) $ primes
--- src/Solver.hs
+++ src/Solver.hs
@@ -35,6 +35,7 @@ import PE034 (solve034)
 import PE035 (solve035)
 import PE037 (solve037)
 import PE038 (solve038)
+import PE041 (solve041)
 import PE048 (solve048)
 import PE067 (solve067)
 
@@ -48,6 +49,7 @@ solvers = H.fromList
   , (027, solve027), (028, solve028), (029, solve029), (030, solve030)
   , (032, solve032), (034, solve034), (035, solve035)
   , (037, solve037), (038, solve038)
+  , (041, solve041)
   , (048, solve048)
   , (067, solve067)
   ]
