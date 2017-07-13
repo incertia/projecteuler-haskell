f1e1d7fd61e7f442af73b9dfbd0e32f19aad949e
Will Song <songw@ainfosec.com>
2017-07-13 12:48:39 -0600

    remove excessive calls to snd and minimumBy

--- src/PE062.hs
+++ src/PE062.hs
@@ -6,7 +6,7 @@ import Data.Ord (comparing)
 
 -- 10000 is just a bound, not sure how to make this work on an infinite list
 solve062 :: String -> Integer
-solve062 _ = snd . minimumBy (comparing snd) . fmap (minimumBy (comparing snd))
+solve062 _ = minimum . fmap (snd . minimumBy (comparing snd))
                . filter ((==5) . length) . groupBy (((==EQ) .) . comparing fst)
                . sortBy (comparing fst) . fmap (sort . show &&& id) $ cubes
   where cubes = (^3) <$> [1..10000]
