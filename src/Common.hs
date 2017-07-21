8ca1dce185024040c3a90d6171d1bb1b8274fcbd
Will Song <incertia@incertia.net>
2017-07-21 14:54:31 -0600

    move cfrac to Common

--- src/Common.hs
+++ src/Common.hs
@@ -2,6 +2,7 @@ module Common where
 
 import Data.Bits (shiftR)
 import Data.Ratio (Ratio, numerator, (%))
+import Math.NumberTheory.Powers (integerSquareRoot, isSquare)
 
 -- (iroot n)^2 = iroot n^2 is confusing so give it a name
 hasiroot :: Integer -> Bool
@@ -21,6 +22,17 @@ iroot n = case n `compare` 0 of
                                  else (op, res)
                 one'  = one `shiftR` 2
                 res'' = res' `shiftR` 1
+
+cfrac :: Integer -> [Integer]
+cfrac n
+  | isSquare n = [a0]
+  | otherwise  = [a_i | (_, _, a_i) <- stuff]
+  where stuff = iterate next (0, 1, a0)
+        a0 = integerSquareRoot n
+        next (m, d, a) = (m', d', a')
+          where m' = d * a - m
+                d' = (n - m'^2) `div` d
+                a' = (a0 + m') `div` d'
 
 conv :: Integer -> [Integer] -> Ratio Integer
 conv 1 (x:_) = x % 1
--- src/PE066.hs
+++ src/PE066.hs
@@ -1,25 +1,14 @@
 module PE066 (solve066) where
 
-import Common (conv)
+import Common (cfrac, conv)
 import Control.Arrow ((&&&))
 import Data.List (maximumBy)
 import Data.Ord (comparing)
 import Data.Ratio (numerator, denominator)
-import Math.NumberTheory.Powers (integerSquareRoot, isSquare)
+import Math.NumberTheory.Powers (isSquare)
 
 -- this is just pell's equation x^2 - Dy^2 = 1
 -- minimal x can be found by substituting x/y from convergents of sqrt D
-
-cfrac :: Integer -> [Integer]
-cfrac n
-  | isSquare n = [a0]
-  | otherwise  = [a_i | (_, _, a_i) <- stuff]
-  where stuff = iterate next (0, 1, a0)
-        a0 = integerSquareRoot n
-        next (m, d, a) = (m', d', a')
-          where m' = d * a - m
-                d' = (n - m'^2) `div` d
-                a' = (a0 + m') `div` d'
 
 fundamental :: Integer -> (Integer, Integer)
 fundamental d = head [(x, y) | (x, y) <- split <$> sequence convergents cf',
