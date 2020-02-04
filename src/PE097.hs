b5f523e8fa78e5dcd7bc54b7ab111a7a1069aeb0
Will Song <will.song@trailofbits.com>
2020-02-04 10:53:44 -0500

    use Mod

--- src/PE097.hs
+++ src/PE097.hs
@@ -1,8 +1,8 @@
+{-# LANGUAGE DataKinds #-}
+
 module PE097 (solve097) where
 
-import Math.NumberTheory.Moduli (modulo, powSomeMod, getVal, SomeMod(..))
+import Math.NumberTheory.Moduli (Mod, powMod, getVal)
 
 solve097 :: String -> Integer
-solve097 _ = case 28433 * powSomeMod (2 `modulo` 10^10) 7830457 + 1 of
-                  SomeMod k -> getVal k
-                  _         -> error "impossible"
+solve097 _ = getVal $ 28433 * powMod (2 :: Mod 10000000000) 7830457 + 1
